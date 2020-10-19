d5d43d371167f07ddbeebe31aa578ca861931a75
william-dvlpr <william.dvlpr@gmail.com>
2020-10-19 02:22:36 -0300

    popup

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,10 +1,29 @@
-const map = L.map('mapid').setView([51.505, -0.09], 15);
+// craeate map
+const map = L.map('mapid').setView([-18.9165168, -48.2848777], 12);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'})
+.addTo(map);
 
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconsAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.hmtl?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>')
+
+// create and add marker
 L
-.marker([51.5, -0.09]).addTo(map)
-.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-.openPopup();
+.marker([-18.9165168, -48.2848777], { icon })
+.addTo(map)
+.bindPopup(popup)
+
